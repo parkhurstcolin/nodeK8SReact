ef27e7a3a3d9c64ea7eb32a0df83c462e3542ced
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-03-27 11:26:40 -0400

    16

--- blog-boilerplate/comments/index.js
+++ blog-boilerplate/comments/index.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { randonBytes, randomBytes } = require('crypto');
+const { randomBytes } = require("crypto");
 
 const app = express();
 app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (res, req) => {
-    res.send(commentsByPostId[req.params.id] || [])
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (res, req) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
 
-    const comments = commentsByPostId[req.params.id] || [];
+  const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({id: commentId, content});
-    
-    commentsByPostId[req.params.id] = comments;
+  comments.push({ id: commentId, content });
 
-    res.status(201).send(comments)
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
