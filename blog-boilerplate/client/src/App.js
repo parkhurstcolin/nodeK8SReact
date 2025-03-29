781f3147be3729f604e3ae54ad9cda90c24ec208
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-03-29 10:11:13 -0400

    25

--- blog-boilerplate/client/src/App.js
+++ blog-boilerplate/client/src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 import PostCreate from "./PostCreate";
-import PostList from './PostList'
+import PostList from "./PostList";
 
-function App() {
+const App = () => {
   return (
     <div className="container">
       <h1>Create Post</h1>
-        <PostCreate />
-        <hr />
-        <h1>Posts</h1>
-        <PostList />
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
-}
-
+};
 export default App;
--- blog-boilerplate/client/src/PostCreate.js
+++ blog-boilerplate/client/src/PostCreate.js
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
+import React, { useState } from "react";
+import axios from "axios";
 
 const PostCreate = () => {
-    const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
-    async function onSubmit (event) {
-        event.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-        await axios.post('http://localhost:4000/posts', {
-            title
-        });
-        
-        setTitle('');
-    }
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
 
+    setTitle("");
+  };
 
-    return <div>
-        <form onSubmit={onSubmit}>
-            <div className='form-group'>
-                <label>Title</label>
-                <input value={title} onChange={e => setTitle(e.target.value)} className="form-control" />
-            </div>
-            <button className='btn btn-primary'>Submit</button>
-        </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
-}
+  );
+};
 
 export default PostCreate;
