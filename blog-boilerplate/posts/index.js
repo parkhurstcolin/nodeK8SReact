9b93d490add1323e68e6198c0537dc15959033d5
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-03-29 11:30:03 -0400

    32

--- blog-boilerplate/posts/index.js
+++ blog-boilerplate/posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto');
 const cors = require("cors");
+const axios = require("axios");
  
 
 const app = express();
@@ -14,16 +15,25 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id] = {
-        id, title
-    };
+  posts[id] = {
+    id,
+    title,
+  };
 
-    res.status(201).send(posts[id]);
-})
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
     console.log("Listening on 4000 for Posts");
