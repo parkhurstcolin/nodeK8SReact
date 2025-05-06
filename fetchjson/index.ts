8ff2c6be2a911ed6611383ee45dc549f4dd7c5b3
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-06 15:59:21 -0400

    587

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,16 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
-  const todo = response.data;
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const ID = todo.id;
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
   const title = todo.title;
-  const finished = todo.finished;
+  const completed = todo.completed;
 
   console.log(`
-      The Todo with ID: ${ID}
+      The Todo with ID: ${id}
       Has a title of: ${title}
-      Is it finished? ${finished}
+      Is it completed? ${completed}
     `);
 });
