c21bd9847455b39780c78a7558cf35b9b73f0453
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-06 16:04:16 -0400

    588

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -8,16 +8,20 @@ interface Todo {
   completed: boolean;
 }
 
-axios.get(url).then(response => {
+axios.get(url).then((response) => {
   const todo = response.data as Todo;
 
   const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
 
-  console.log(`
-      The Todo with ID: ${id}
-      Has a title of: ${title}
-      Is it completed? ${completed}
-    `);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it completed? ${completed}
+  `);
+};
