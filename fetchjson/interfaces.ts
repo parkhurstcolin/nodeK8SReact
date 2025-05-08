0efef3900e665ec865315c5c3d3f2e500dc96239
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-08 11:24:01 -0400

    618

---
+++ fetchjson/interfaces.ts
@@ -0,0 +1,14 @@
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true
+} ;
+
+function printVehicle(vehicle: {name: string; year: number; broken: boolean}): void{
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken: ${vehicle.broken}`);
+  
+}
+
+printVehicle(oldCivic);
