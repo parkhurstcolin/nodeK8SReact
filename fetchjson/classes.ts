1690f0a9ac40b25b36dd21164d93d296e83f112e
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-09 11:03:10 -0400

    628

--- fetchjson/classes.ts
+++ fetchjson/classes.ts
@@ -13,16 +13,19 @@ class Vehicle {
 const vehicle = new Vehicle('orange');
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//   private drive(): void {
-//     console.log('vroom');
-//   }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+  private drive(): void {
+    console.log('vroom');
+  }
 
-//   startDrivingProcess(): void {
-//     this.drive();
-//     this.honk();
-//   }
-// }
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
 
-//const car = new Vehicle();
-//car.startDrivingProcess();
+const car = new Car(4, 'Red');
+car.startDrivingProcess();
