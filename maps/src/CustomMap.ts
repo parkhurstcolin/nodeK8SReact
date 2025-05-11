d066a72c9232ba04a1e95ea4d3c1623937f2a9e6
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-11 11:12:48 -0400

    647

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,15 +1,29 @@
 /// <reference types="@types/google.maps" />
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
-  private googleMap: google.maps.Map
+  private googleMap: google.maps.Map;
 
   constructor(divId: string) {
-    this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
-      zoom: 1,
-      center: {
-        lat: 0,
-        lng: 0
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
       }
-    });
-    
+    );
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: user.location,
+    });
+  }
+
+  addCompanyMarker(company: Company): void {}
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,7 @@
-//import { User } from './User';
+import { User } from './User';
 //import { Company } from './Company';
 import { CustomMap } from './CustomMap';
+const user = new User();
+const customMap = new CustomMap('map');
 
-new CustomMap('map');
+customMap.addUserMarker(user);
