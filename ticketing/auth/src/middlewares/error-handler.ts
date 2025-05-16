7a3fea58b1621aa4ecfc200415d68309e6d1e628
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-16 10:31:34 -0400

    144

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -4,14 +4,19 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof RequestValidationError) {
-    console.log('handling this error as a request validation error');
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === 'field') {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    console.log('handling this error as a database connection error');
+    return res.status(500).send({ errors: [{ message: err.reason }] });
   }
 
   res.status(400).send({
-    message: err.message,
+    errors: [{ message: 'Something went wrong' }],
   });
 }
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -11,8 +11,8 @@ router.post(
     body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters'),
+      .isLength({ min: 5, max: 20 })
+      .withMessage('Password must be between 5 and 20 characters'),
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
