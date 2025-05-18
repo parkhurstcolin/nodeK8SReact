5f29498f176200813314b398afc6d8a4c99dcdb7
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-18 10:01:33 -0400

    157

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -7,6 +7,12 @@ interface UserAttrs {
   password: string;
 }
 
+//  An interface that describes the properties
+//  that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any
+}
+
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -18,13 +24,11 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 })
-
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
 
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
 
-export { User, buildUser }
+export { User }
