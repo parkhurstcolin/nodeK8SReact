85b54c2abe3c6e55fc4defaa1f7099cd198a8aa6
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-29 11:15:05 -0400

    210

--- ticketing/auth/src/routes/__test__/current-user.test.ts
+++ ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -2,17 +2,23 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('responds with details about the current user', async () => {
-  await request(app)
+  const authResponse = await request(app)
     .post('/api/users/signup')
     .send({
       email: 'test@test.com',
-      password: 'password'
+      password: 'password',
     })
     .expect(201);
+  const cookie = authResponse.get('Set-Cookie');
 
+  if (!cookie) {
+    throw new Error('Cookie not set after signup');
+  }
   const response = await request(app)
     .get('/api/users/currentuser')
+    .set('Cookie', cookie)
     .send()
     .expect(200);
-    
+
+  expect(response.body.currentUser.email).toEqual('test@test.com');
 })
