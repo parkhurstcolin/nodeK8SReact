f07849bef1931c3c452bfa5449ac0f8bf5251723
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-29 10:32:47 -0400

    203

--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,35 @@ it('returns a 201 on succesful signup', async () => {
     })
     .expect(201)
 })
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'testtest.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'abc',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email & password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'testtest.com' })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'p' })
+    .expect(400);
+});
