6766e1e5760a3d9c7d887528775c1626f8a208bf
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-29 11:26:49 -0400

    213

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,20 +1,25 @@
 import {MongoMemoryServer} from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app'
+import request from 'supertest';
+import { app } from '../app';
 
-let mongo: any
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
+let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'aikufgahseoighjwe';
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
-  
+
   await mongoose.connect(mongoUri, {});
-})
+});
 
 beforeEach(async () => {
   if (mongoose.connection.db) {
     const collections = await mongoose.connection.db.collections();
- 
+
     for (let collection of collections) {
       await collection.deleteMany({});
     }
@@ -27,3 +32,23 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  if (!cookie) {
+    throw new Error('Failed to get cookie from response');
+  }
+  return cookie;
+};
