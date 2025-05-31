6141b4bd67bf947371035ca66facbe9d29e0a824
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-05-31 10:35:50 -0400

    224

---
+++ ticketing/client/pages/auth/signup.js
@@ -0,0 +1,14 @@
+export default () => {
+  return <form>
+    <h1>Sign up</h1>
+    <div className="form-group">
+      <label>Email Address</label>
+      <input className='form-control'></input>
+    </div>
+    <div className="form-group">
+      <label>Password</label>
+      <input type='password' className='form-control'></input>
+    </div>
+    <button className='btn btn-primary' >Sign Up</button>
+  </form>
+}
