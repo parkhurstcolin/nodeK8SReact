1da94baf2654b6bfb7b7b143129aff1cd104d626
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-01 10:44:38 -0400

    228

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/useRequest';
 
 export default () => {
   const [email, setEmail] = useState('');
@@ -40,14 +41,7 @@ export default () => {
         ></input>
       </div>
       {errors.length > 0 && (
-        <div className='alert alert-danger'>
-          <h4>Ooops...</h4>
-          <ul className='my-0'>
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
+        
       )}
       <button className='btn btn-primary'>Sign Up</button>
     </form>
