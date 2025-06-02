12b1fd4cf016d97b4afcb1b8140dd01817817b74
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-02 11:08:33 -0400

    232

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,7 +1,10 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component', color);
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+ 
   return <h1>Landing Page</h1>;
-};
 
 LandingPage.getInitialProps = () => {
   console.log('I am on the server');
