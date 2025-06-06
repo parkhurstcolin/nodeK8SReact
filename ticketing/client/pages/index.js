c7551292b3317f9c6617778fc9b07872fc932678
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-06 09:30:41 -0400

    237

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => {
+  //   console.log(err.message);
+  // });
 
   return <h1>Landing Page</h1>;
 };
