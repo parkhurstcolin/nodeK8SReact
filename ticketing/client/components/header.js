5522e31a422be1e862412b9f70fa68f4d1123008
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-07 11:09:47 -0400

    252

--- ticketing/client/components/header.js
+++ ticketing/client/components/header.js
@@ -1,16 +1,30 @@
 import Link from "next/link";
 
 export default ({ currentUser }) => {
-  return <nav className="navbar navbar-light bg-light">
-    <Link className="navbar-brand" href="/">
-      GitTix
-    </Link>
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href}>
+          <Link href={href}>{label}</Link>
+        </li>
+      );
+    });
 
-    <div className="d-flex justify-content-end">
-      <ul className="nav d-flex align-items-center">
-        {currentUser ? "Sign out" : "Sign in/up"}
-      </ul>
-    </div> 
-  </nav>
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <Link className='navbar-brand' href='/'>
+        GitTix
+      </Link>
+
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
+      </div>
+    </nav>
+  );
 }
 
