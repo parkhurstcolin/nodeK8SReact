e76dabd22ebb67c80937f5ef50fb6a756040dc48
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-13 11:26:05 -0400

    271

---
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+  
+})
+it('can only be accessed if the user is singed in', async () => {
+  
+})
+it('return an error if an invalid title is provided', async () => {
+  
+})
+it('return an error if an invalid price is provided', async () => {
+  
+})
+it('creates a ticket with valid inputs', async () => {
+  
+})
