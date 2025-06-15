3be62c92e6d00e788f0a5bebcc8733957771b777
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-15 10:22:54 -0400

    278

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,10 +1,23 @@
-import express, { Request, Response } from 'express'
-import { requireAuth } from '@xycorp/common';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@xycorp/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
 
-export { router as createTicketRouter}
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
+
+export { router as createTicketRouter };
