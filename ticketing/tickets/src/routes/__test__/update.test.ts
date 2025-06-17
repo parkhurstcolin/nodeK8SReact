d945405b0e2bbbfe01f6c5d55f905886dc85d9d3
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-17 14:28:00 -0400

    290

--- ticketing/tickets/src/routes/__test__/update.test.ts
+++ ticketing/tickets/src/routes/__test__/update.test.ts
@@ -1,30 +1,30 @@
 import request from "supertest"
 import { app } from "../../app"
 import mongoose from "mongoose"
+import { contextsKey } from 'express-validator/lib/base';
 
 it('returns a 404 if the provided id does not exist', async () => {
-  const id = new mongoose.Types.ObjectId().toHexString()
+  const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
     .put(`/api/tickets/${id}`)
     .set('Cookie', global.signin())
     .send({
-      title: "concert",
-      price: 20
+      title: 'concert',
+      price: 20,
     })
-    .expect(404)
-})
+    .expect(404);
+});
 
 it('returns a 401 if the user is not authenticated', async () => {
-  const id = new mongoose.Types.ObjectId().toHexString()
+  const id = new mongoose.Types.ObjectId().toHexString();
   await request(app)
     .put(`/api/tickets/${id}`)
     .send({
-      title: "concert",
-      price: 20
+      title: 'concert',
+      price: 20,
     })
-    .expect(401)
-  
-})
+    .expect(401);
+});
 
 it('returns a 401 if the user does not own the ticket', async () => {
   const response = await request(app)
@@ -76,5 +76,29 @@ it('returns a 400 if the user does not provide an invalid title or price', async
 });
 
 it('updates the ticket provided valid inputs', async () => {
-  
-})
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets/')
+    .set('Cookie', cookie)
+    .send({
+      title: 'concert',
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'movie',
+      price: 15,
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('movie');
+  expect(ticketResponse.body.price).toEqual(15);
+});
