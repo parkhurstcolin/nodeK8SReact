4fab96f423e05374435678e1ec22f0e4db2b5609
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-19 11:11:47 -0400

    299

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,4 +6,14 @@ const stan = nats.connect("ticketing", 'abc', {
 
 stan.on('connect', () => {
   console.log('Publisher connected to NATS')
+  
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:create', data, () => {
+    console.log('Event published');
+  });
 })
