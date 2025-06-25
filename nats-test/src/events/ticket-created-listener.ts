815f61ab255b5524da4932c1549cabd8c77a923d
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-06-25 00:14:00 -0400

    321

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,6 +4,7 @@ import { Listener } from "./base-listener";
 export class TicketCreatedListener extends Listener {
   subject = 'ticket:created';
   queueGroupName = 'payments-service';
+
   onMessage(data: any, msg: Message) {
     console.log('Event data!', data);
 
