9f19f67ba5a77a60f0749982ef057c332cb0ca74
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-07-17 11:16:50 -0400

    364

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { NotFoundError, requireAuth, validateRequest } from '@xycorp/common';
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@xycorp/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
+import { OrderStatus } from '@xycorp/common';
 
 const router = express.Router();
 
@@ -28,6 +34,23 @@ router.post(
     }
 
     // Make sure that this ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket we just found *and* the order stauts is *not* cancelled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+        ],
+      },
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
 
     // Calculate an expiration date for this order
 
