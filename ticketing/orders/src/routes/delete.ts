06714616cb5db8342c7f19561f63287d22aa3956
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-07-23 19:43:26 -0400

    376

--- ticketing/orders/src/routes/delete.ts
+++ ticketing/orders/src/routes/delete.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@xycorp/common';
+import { Order, OrderStatus } from '../models/order';
+const router = express.Router();
 
-const router = express.Router()
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({})
-})
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter }
