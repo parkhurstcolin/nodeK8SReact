fb16441fa237e7bf4671a3bb55d8ad38da362f8f
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-07-24 11:03:03 -0400

    384

--- ticketing/orders/src/routes/__test__/delete.test.ts
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -32,4 +32,28 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
 
-it.todo('emit a order cancelled event')
+it('emit a order cancelled event', async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
