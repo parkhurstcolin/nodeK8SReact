64938c8ee9771de4861a660707903a575aa96cef
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-07-25 11:17:26 -0400

    389

--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -1,12 +1,11 @@
 import { Message } from 'node-nats-streaming'
 import { Subjects, Listener, TicketCreatedEvent} from '@xycorp/common'
 import { Ticket } from '../../models/ticket'
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName: string = 'orders-service';
+  queueGroupName: string = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
-    
-  }
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {}
 }
