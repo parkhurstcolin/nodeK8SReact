44a3c07f1c1a70de08aab872e6363190272f9dab
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-08-17 11:21:41 -0400

    424

--- ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
+++ ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -2,7 +2,7 @@ import { OrderCreatedEvent, OrderStatus } from '@xycorp/common';
 import { OrderCreatedListener } from '../order-created-listener';
 import { natsWrapper } from '../../../nats-wrapper';
 import { Ticket } from '../../../models/ticket';
-import mongoose from 'mongoose';
+import mongoose, { set } from 'mongoose';
 import { Message } from 'node-nats-streaming';
 
 const setup = async () => {
@@ -37,3 +37,18 @@ const setup = async () => {
 
   return { listener, ticket, data, msg };
 };
+
+it('sets the userId of the ticket', async () => {
+  const { listener, ticket, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it('acks the message', async () => {
+  const { listener, ticket, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled()
+});
