45e70cefd2eadc42b206d82864ac9e92e5fdf81c
Colin Parkhurst <parkhurstcolin@protonmail.com>
2025-08-17 11:08:37 -0400

    422

--- ticketing/tickets/src/events/listeners/order-created-listener.ts
+++ ticketing/tickets/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,28 @@
 import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@xycorp/common";
 import { queueGroupName } from "./queue-group-name";
+import { Ticket } from '../../models/ticket';
 
-export class OrderCreatedListener extends Listener <OrderCreatedEvent>{
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message): void {
-    
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Find the ticket that the ordr is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // Mark the ticket as being reserved by settings its orderId property
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
   }
 }
